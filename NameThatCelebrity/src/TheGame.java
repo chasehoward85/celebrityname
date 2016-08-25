7287c4847b0776f013f648d0a47176b53fe81bd7
Chase Howard <chasehoward85@gmail.com>
2016-08-25 11:56:46 -0600

    Celebrity Name Game

---
+++ NameThatCelebrity/src/TheGame.java
@@ -0,0 +1,19 @@
+
+public class TheGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Allan Alda";
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck";
+		
+		String c1 = s1.substring(2, 7);
+		String c2 = s2.substring(2 ,7);
+		String c3 = s3.substring(2, 9);
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+	}
+
+}
